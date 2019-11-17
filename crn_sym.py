d066fe0d68c27cc28a25202a8cc079456b96b073
Rithvik Panch <rithvikP@users.noreply.github.com>
2019-11-17 13:02:58 -0800

    Add basic structure

---
+++ crn_sym.py
@@ -0,0 +1,31 @@
+class RxnSystem:
+    def get_ode_functions(self):
+        pass
+    
+    def get_ode_string(self):
+        pass
+
+    def get_symbols(self):
+        pass
+
+class Rxn:
+    pass
+
+class RevRxn:
+    pass
+
+class Term:
+    pass
+
+class Schedule:
+    pass
+
+class Conc(Schedule):
+    pass
+
+class ConcEq:
+    pass
+
+class ConcDiffEq(ConcEq):
+    pass
+
---
+++ solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def get_sols(self):
+        pass
+    
+    def basic_graph(self):
+        pass
+
+    def get_final_state(self):
+        pass
+
+    def get_var_sols(self, *vars):
+        pass
+
+def solve_ode(odes, time):
+    pass
+
