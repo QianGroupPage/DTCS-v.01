6cab4f33c25bea151f4c8bad8031b9d9c16575aa
Ye <wangye@berkeley.edu>
2020-06-25 16:57:55 -0700

    Import the code to adapt syntax into surface crn engine

---
+++ lblcrn/surface_crn/api_adapter/api_adapt.py
@@ -0,0 +1,68 @@
+from lblcrn.crn_sym import Rxn, RevRxn, Surface, SurfaceRxn, SurfaceRevRxn
+import re
+import random
+
+
+def to_str(rule):
+    """
+    :param rule: a reaction rule
+    :return: a list of strings representing the rule, suitable for Surface CRN
+    """
+    if isinstance(rule, RevRxn):
+        return [f"({rule.rate_constant}) {rule.reactants} -> {rule.products}",
+                f"({rule.rate_constant_reverse}) {rule.reactants} -> {rule.products}"]
+    elif isinstance(rule, Rxn):
+        return [f"({rule.rate_constant}) {rule.reactants} -> {rule.products}"]
+    elif isinstance(rule, SurfaceRxn):
+        left = "+".join(rule.reactants)
+        right = "+".join(rule.products)
+        return [f"({rule.rate_constant}) {left} -> {right}"]
+    elif isinstance(rule, SurfaceRevRxn):
+        left = "+".join(rule.reactants)
+        right = "+".join(rule.products)
+        return [f"({rule.rate_constant}) {left} -> {right}",
+                f"({rule.rate_constant_reverse}) {right} -> {left}"]
+    else:
+        raise Exception("Rule is not of type crn_sym.Rxn or crn_sym.RevRxn.")
+
+
+def generate_rules(rsys):
+    """
+    :param rsys: a reaction system object
+    :return: a list of strings representing the rule, suitable for Surface CRN
+    """
+    reactions = []
+    for c in rsys.components:
+        if isinstance(c, Rxn):
+            reactions.extend(to_str(c))
+    return reactions
+
+
+def generate_initial_surface(rsys, random_seed=30):
+    """
+    :rsys: a reaction system object
+    :return: a string representing the initial surface.
+    """
+    species = []
+    for c in rsys.conc_eqs:
+        # TODO
+        if not re.match('[1-9][0-9]*', str(c.expression)):
+            raise Exception(f"{c.symbol} must have a positive integer number of initial counts. Currently, " +
+                            f"it's initial count is {c.expression}")
+        species.extend([str(c.symbol) for _ in range(int(str(c.expression)))])
+
+    if not rsys.surface:
+        rsys.surface = Surface('surface', (10, 10), color=(34, 139, 34))
+        print("Using a default surface")
+
+    rows = rsys.surface.size[0]
+    cols = rsys.surface.size[1]
+
+    random.seed(random_seed)
+    random_indices = random.sample(range(rows * cols), len(species))
+
+    surface_strs = [[rsys.surface.name for _ in range(cols)] for _ in range(rows)]
+    for i, choice in enumerate(random_indices):
+        surface_strs[choice // cols][choice % cols] = species[i]
+
+    return "\n".join([" ".join(strs) for strs in surface_strs]) + "\n"
