f80f99ba4ad2c8cfc572c89c9ea5c7d3a7fc17b9
Ye <wangye@berkeley.edu>
2020-11-13 16:47:36 -0800

    Visualize all peak fitting results for a RawExperiment
    PeakFit and Curve objects' plotting methods now take in ax as an
    argument.

--- lblcrn/common/curve.py
+++ lblcrn/common/curve.py
@@ -12,6 +12,15 @@ class Curve:
     def __init__(self, function, start, stop, n_samples,
                  original_curve_df,
                  *params):
+        """
+
+        :param function:
+        :param start:
+        :param stop:
+        :param n_samples:
+        :param original_curve_df:
+        :param params:
+        """
         self.f = function
         self.start, self.stop, self.n_samples = start, stop, n_samples
         self.params = params
@@ -20,19 +29,16 @@ class Curve:
         self.ys = np.zeros(n_samples)
 
         for p in zip(*params):
+            print("parameter  set", p)
             self.ys += self.f(self.xs, *p)
-
-        # for p in zip(*params):
-        #     print(p)
-        # print(self.ys)
 
         self.original_curve_df = original_curve_df
 
-    def plot(self, target=None, data_fmt='b.', species_fmt='r--', verbose=True, data=False):
-        if not target:
-            target = plt.gca()
+    def plot(self, ax=None, data_fmt='b.', species_fmt='r--', verbose=True, data=False):
+        if not ax:
+            ax= plt.gca()
         # if data:  # Plot Data
-        target.plot(self.xs, self.ys, data_fmt, label="Fitted Sum")
+        ax.plot(self.xs, self.ys, data_fmt, label="Fitted Sum")
 
         for i, d in enumerate(zip(*self.params)):
             if verbose:
@@ -40,10 +46,9 @@ class Curve:
             else:
                 label = 'Component {}'.format(i)
             # Plot Species
+            ax.plot(self.xs, self.f(self.xs, *d), species_fmt, label=label)
+        ax.plot(self.original_curve_df.index, self.original_curve_df.iloc[:, 0], label="Original Curve")
 
-            target.plot(self.xs, self.f(self.xs, *d), species_fmt, label=label)
-        target.plot(self.original_curve_df.index, self.original_curve_df.iloc[:, 0], label="Original Curve")
-
-        target.set_xlim(max(max(self.xs), self.original_curve_df.index.max()),
+        ax.set_xlim(max(max(self.xs), self.original_curve_df.index.max()),
                         min(min(self.xs), self.original_curve_df.index.min()))
-        target.legend(loc='upper center', bbox_to_anchor=(0.5, 1.3), fancybox=True)
+        ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.3), fancybox=True)
